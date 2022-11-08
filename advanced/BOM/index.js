b1f0bc1de0e710bed533614f3aa478d2b270a30a
Divyansh Dixir <divyanshdixit96@gmail.com>
2022-11-09 00:19:03 +0530

    dom comp, bom

---
+++ advanced/BOM/index.js
@@ -0,0 +1,36 @@
+// browser 
+
+var t = 10;
+function hj(){
+
+}
+var obj = {
+
+}
+console.log(hj())
+console.log(window.hj())
+console.log(t)
+console.log(window.t)
+
+// global variable are props to window object
+// global functions are methods to window object
+
+// window size: innerHeight, innerWidth, 
+// outerWidth, outerHeight => size of browser window (including toolbars/scrollbars)
+
+// window.close() => close current window 
+// window.open('url', name=target, spec (scrollbars, width, height, left, top, toolbar)) => open a new window.
+// window.moveTo(x,y) => move to specified postion => x => horizontal, y - vertical
+// window.resizeTo(width, height) => reszies window to new height and width
+
+// window.screen => contains info about user's screeen.
+
+
+// window.location => 
+// current page url, 
+// redirection
+
+// window.location.href => return the url of current page.
+// window.location.hostname => retunr domain name
+// window.location.pathname => retunr pathname or filename
+// window.location.protocol => reutnr 
