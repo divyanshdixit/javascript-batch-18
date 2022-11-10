327b76065c164e692de3eadbea962b5c1ba3494a
Divyansh Dixir <divyanshdixit96@gmail.com>
2022-11-11 01:02:43 +0530

    sdds

--- advanced/BOM/index.js
+++ advanced/BOM/index.js
@@ -8,9 +8,9 @@ var obj = {
 
 }
 console.log(hj())
-console.log(window.hj())
+// console.log(window.hj())
 console.log(t)
-console.log(window.t)
+// console.log(window.t)
 
 // global variable are props to window object
 // global functions are methods to window object
@@ -33,4 +33,34 @@ console.log(window.t)
 // window.location.href => return the url of current page.
 // window.location.hostname => retunr domain name
 // window.location.pathname => retunr pathname or filename
-// window.location.protocol => reutnr 
+// window.location.protocol => return the erb based protocol
+// window.location.assign(newUrl) => loads new document
+// window.location.replace(newUrl) => current page will not be saved in session history  , you can not go back to previous url. 
+// window.location.port => reutnr the port number (80 for http, 443 for https)
+// window.location.reload() => refersh the page.
+// location.toString()
+// location.search => get query params '?q=1'
+
+// parsing : tomorrow 
+let params = new URLSearchParams(location.search)
+let res = parseInt(params.get('q')) // string
+// history => object:
+
+// window.history => browser history
+
+// history.back() => load previous url in history list
+// history.forward()
+// history.go(-1)
+// history.pushState(state:object, title:'document.title', url ) => add a new entry to browser session history;
+
+// query parameters:
+const url = new URL(location);
+console.log(url);
+
+url.searchParams.set('q', 'query');
+
+// window.navigator: contains info abt users' browser
+
+navigator.appName
+// navigator.platform
+// navigator.online => check if broser is online or not;
